7fbeb7ac50484e7695cdb845b7ebb6122f9d10ac
serena <sijing.c@gmail.com>
2022-07-06 22:55:39 -0400

    add styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss'
+
 const App = () => {
   const categories = [
     {
@@ -25,17 +27,16 @@ const App = () => {
   return (
     <div className='categories-container'>
       {
-        categories.map((category) => {
-          <div className='category-container'>
-            {/* <img /> */}
+        categories.map((category) => (
+          <div className='category-container' key={category.id}>
+            <div className='background-image' />
             <div className='category-body-container'>
               <h2>{category.name}</h2>
               <p>Shop Now</p>
             </div>
-          </div>;
-        })
+          </div>
+        ))
       }
-
     </div>
   );
 };
